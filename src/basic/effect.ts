7b9478b53763e7c28ea344039c4d8b91e2836282
Cosimo Matteini <dev.matteini@gmail.com>
2023-02-22 23:09:28 +0100

    add basic module

---
+++ src/basic/effect.ts
@@ -0,0 +1,30 @@
+import * as Effect from "@effect/io/Effect"
+import * as F from "@effect/data/Function"
+import * as E from "@effect/data/Either"
+
+const s = Effect.succeed(7) // Effect.Effect<never, never, number>
+
+const f = Effect.fail(3) // Effect.Effect<never, number, never>
+
+const ss = Effect.sync(() => {
+    console.log("hello from Effect.sync")
+    return 4
+}) // Effect.Effect<never, never, number>
+
+const sf = Effect.failSync(() => {
+    console.log("hello from Effect.failSync")
+    return 4
+}) // Effect.Effect<never, number, never>
+
+const eitherFromRandom = (random: number): E.Either<string, number> =>
+    random > 0.5 ? E.right(random) : E.left("Number is less than 0.5")
+
+const x = F.pipe(
+    Effect.sync(() => Math.random()), // Effect.Effect<never, never, number>
+    Effect.map(eitherFromRandom), // Effect.Effect<never, never, Either<string, number>>
+    Effect.flatMap(Effect.fromEither), // Effect.Effect<never, string, number>
+    // or Effect.absolve
+)
+
+// Run Effect (can also run it as Promise)
+console.log(Effect.runSync(x))
---
+++ src/basic/fp-ts.ts
@@ -0,0 +1,17 @@
+import * as F from "fp-ts/function"
+import * as E from "fp-ts/Either"
+
+const r = E.right(7)
+
+const l = E.left(3)
+
+const eitherFromRandom = (random: number): E.Either<string, number> =>
+    random > 0.5 ? E.right(random) : E.left("Number is less than 0.5")
+
+const x = F.pipe(
+    // keep new line
+    Math.random(),
+    eitherFromRandom,
+)
+
+console.log(x)
