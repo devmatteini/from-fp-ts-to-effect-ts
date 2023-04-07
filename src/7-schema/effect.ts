8995ee15bc543d3fb780645667fbdd42b742e598
Cosimo Matteini <dev.matteini@gmail.com>
2023-04-07 16:11:52 +0200

    7-schema: add schema annotations

--- src/7-schema/effect.ts
+++ src/7-schema/effect.ts
@@ -1,9 +1,10 @@
 import * as F from "@effect/data/Function"
 import * as E from "@effect/data/Either"
 import * as S from "@effect/schema/Schema"
+import * as AST from "@effect/schema/AST"
 import { formatErrors } from "@effect/schema/TreeFormatter"
 import * as PR from "@effect/schema/ParseResult"
-import { PathReporter } from "io-ts/PathReporter"
+import * as O from "@effect/data/Option"
 
 // ********** Branded types **********
 // https://github.com/Effect-TS/schema#branded-types
@@ -51,3 +52,40 @@ const dateFromString = F.pipe(
     }),
 )
 console.log("DateFromString from decode: ", dateFromString)
+
+// ********** Annotations **********
+// https://github.com/Effect-TS/schema#annotations
+
+// https://www.emailregex.com/
+const emailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+const Email = F.pipe(
+    S.string,
+    S.nonEmpty(),
+    S.pattern(emailRegex, {
+        // Specify human-readable message for this step instead of the regex
+        message: (x) => `Invalid email pattern '${x}'`,
+        examples: ["info@example.com", "foo.bar@example.co.uk"],
+    }),
+    S.identifier("Email"),
+)
+
+const invalidEmail = F.pipe(
+    S.parseEither(Email)("any@email"),
+    E.getOrElse((x) => {
+        // We can retrive the 'identifier' annotation to enrich the error message.
+        // This is like using io-ts <Type>.name, but require a bit more work
+        const identifier = F.pipe(
+            AST.getAnnotation<string>(AST.IdentifierAnnotationId)(Email.ast),
+            O.getOrElse(F.constant("unknown schema")),
+        )
+        const examples = F.pipe(
+            AST.getAnnotation<string[]>(AST.ExamplesAnnotationId)(Email.ast),
+            O.match(F.constant(""), (x) => x.join(", ")),
+            (x) => `Examples: ${x}`,
+        )
+        throw new Error(`Parse ${identifier}: ${formatErrors(x.errors)} (${examples})`)
+    }),
+)
+console.log("Email decode with annotation: ", invalidEmail)
